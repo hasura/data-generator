4f12d51dc6efc3ba1bd47db7cd60d1f7a383c52a
kenstott <128912107+kenstott@users.noreply.github.com>
2025-03-11 22:42:31 -0400

    initial commit

--- fsi_data_generator/fsi_generators/security.py
+++ fsi_data_generator/fsi_generators/security.py
@@ -10,9 +10,9 @@ fake = Faker()
 def security(dg):
     return [
         ('security\\.identity_roles', '^identity_id$',
-         lambda a, b, c: fake.unique.random_element(tuple(dg.inserted_pks['security\\.identities']))),
+         lambda a, b, c: fake.unique.random_element(tuple(dg.inserted_pks['security.identities']))),
         ('security\\.identity_roles', '^role_id',
-         lambda a, b, c: fake.unique.random_element(tuple(dg.inserted_pks['security\\.roles']))),
+         lambda a, b, c: fake.unique.random_element(tuple(dg.inserted_pks['security.roles']))),
         ('security\\..*', '^subnet$', generate_correlated_subnet),
         ('security\\..*', '^.*port$', get_random_port_with_popular_bias),
         ('security\\..*', '^tcp_flag', text_list([
